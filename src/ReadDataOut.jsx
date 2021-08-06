1790413a006873f0458b1c673fb4d842c33d3d12
anAwsomePerson <an.awsome.person.99@gmail.com>
2021-08-05 22:57:42 -0700

    re-added src and npm

---
+++ src/ReadDataOut.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Grid} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import {TextField, FormControl, InputLabel, Select} from '@material-ui/core';
+
+const ReadDataOut = () => {
+    return(
+
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <form noValidate autoComplete = "off">
+                    <TextField label = "主持人"/>
+                    <TextField label = "录入人"/>
+                 </form>
+            </Grid>
+            <Grid item xs={12}>
+                <form noValidate>
+                    <TextField  id="date" label="日期"
+                    type="date"
+                    InputLabelProps={{ shrink: true,}}/>
+                </form>
+            </Grid>
+            <Grid item xs = {12}>
+                <FormControl>
+                    <InputLabel htmlFor="age-native-simple">绘本</InputLabel>
+                    <Select native
+                        inputProps={{name: '绘本名称', id: 'age-native-simple',}}>
+                        <option aria-label="None" value="" />
+                        <option value={10}>绘本1</option>
+                        <option value={20}>绘本2</option>
+                        <option value={30}>绘本3</option>
+                        </Select>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+                <Button color="primary">
+                    输出
+                </Button>
+            </Grid>
+      </Grid>
+    );
+}
+export default ReadDataOut;
