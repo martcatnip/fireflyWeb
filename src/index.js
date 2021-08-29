9e47d7a67b59e8e576947d8360c4fde1a45f213b
anAwsomePerson <an.awsome.person.99@gmail.com>
2021-08-28 20:56:19 -0400

    tried to fix 'firebase' is not defined

--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const config = {
+  apiKey: "AIzaSyCMo0j3bEkaMUi6n-ThtGU4PrE4QFf6Vdg",
+  authDomain: "fireflyweb-8ce10.firebaseapp.com",
+  databaseURL: "https://fireflyweb-8ce10-default-rtdb.firebaseio.com",
+  projectId: "fireflyweb-8ce10",
+  storageBucket: "fireflyweb-8ce10.appspot.com",
+  messagingSenderId: "1004666038917",
+  appId: "1:1004666038917:web:ea793065abe6a45f4197ec",
+  measurementId: "G-XVBPFCSS51"
+};
+
+// Initialize Firebase
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+var database = firebase.database();
+const app = initializeApp(config);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
